e2907ef86cecb73ca53c1a0419274a48dba4a42b
Louis C Leon <louistheleon@gmail.com>
2022-06-03 11:20:46 -0700

    change up directory structure; add stuff to setup script

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+source /etc/os-release
 if [[ $OSTYPE == 'darwin'* ]]; then
     echo 'Setting up for macOS'
     which -s brew
@@ -9,5 +10,23 @@ if [[ $OSTYPE == 'darwin'* ]]; then
         echo 'Updating Homebrew'
         brew update
     fi
-    brew install neovim
+    brew install neovim kitty
+elif [[ $PRETTY_NAME == 'Fedora'* ]]; then
+    echo 'Setting up for Fedora Linux'
+    sudo dnf install neovim kitty
+else
+    echo 'This script is only configured for MacOS and Fedora Linux'
+    echo 'Looks like this is a different OS. Sorry!'
+    exit 1
 fi
+
+ln -sfv "$(pwd)/.zshrc" $HOME
+ln -sfv "$(pwd)/.gitconfig" $HOME
+ln -sfv "$(pwd)/global_gitignore" $HOME .gitignore
+ln -sfv "$(pwd)/.config/nvim" $HOME/.config
+ln -sfv "$(pwd)/.config/kitty" $HOME/.config
+ln -sfv "$(pwd)/.config/starship.toml" $HOME/.config
+
+git config --global core.excludesfile ~/.gitignore
+
+exit 0
